065490557198fd4d77c277ebd92fb1d0c811e0d4
Julia-wr <julia.wr59@web.de>
2024-11-27 00:11:41 +0100

    new alerts & minor changes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const keineGleich = 0;
 const zweiGleich = 1.5;
-const dreiGleich = 100;
+const dreiGleich = 10;
+
+
 
 //Changes current money value and saves name
 let currentDisplayMoney = document.getElementById('currentDisplayMoney');
-window.onload = function(){
-    currentDisplayMoney.textContent = "You currently have " + sessionStorage.getItem('money') + "€";
+if(sessionStorage.getItem('money') != null){
+    window.onload = function(){
+        currentDisplayMoney.textContent = "You currently have " + sessionStorage.getItem('money') + "€";
+    }
 }
 
 let buttonCharge = document.getElementById('chargeB');
@@ -23,6 +27,7 @@ function chargeB(event){
     currentDisplayMoney.textContent = "You now have "+ sessionStorage.getItem('money') + "€";
 }
 
+//checks if user put something in the charging boxes
 let wrongInputDisplay = document.getElementById('wrongInputDisplay');
 function checkBoxes(){
     if(document.getElementById('name').value == ""
@@ -31,7 +36,6 @@ function checkBoxes(){
         return false;
     }
     return true;
-        
 }
 
 //Button for 1€ bet starts Spin
@@ -50,7 +54,7 @@ function betHigher1Spin(event) {
 
 //Spin the Slot Machine
 function doSpin(bet) {
-    //!!insert current money check, to see if user has enough money!!
+    
     var slotImages = ["Slot Pics/Seven.jpg", "Slot Pics/Bell.jpg", "Slot Pics/Cherry.jpg", "Slot Pics/Grape.jpg", "Slot Pics/lemon.jpg", "Slot Pics/Orange.jpg", "Slot Pics/Plum.jpg", "Slot Pics/Strawberry.jpg", "Slot Pics/Watermelon.jpg"];
     //create random numbers to select pictures randomly
 	var image1 = Math.floor((Math.random() * 9));
@@ -64,37 +68,38 @@ function doSpin(bet) {
     checkWin(image1, image2, image3, bet);
 
 }
-
+let winLoseDisplay = document.getElementById('winLoseDisplay');
 //Check if Player won and adds win to players wallet or deducts lost bet
 function checkWin(x, y, z, bet) {
+    let win;
     if(x == y && y == z) {
-        sessionStorage.setItem('money', +sessionStorage.getItem('money') + +bet * +dreiGleich);
-        alert('Congratulations you got the jackpot!! You win ' + dreiGleich * bet + '€');
+        win = +bet * +dreiGleich;
+        sessionStorage.setItem('money', +sessionStorage.getItem('money') + +win);
+        winLoseDisplay.textContent = "Congratulations you got the jackpot!! You win " + win + "€";
+        checkHighscore(win);
     }
-    if(x == y || y == z || x == z){
-        sessionStorage.setItem('money', +sessionStorage.getItem('money') + +bet * +zweiGleich);
-        alert('Congratulations! You win ' + zweiGleich * bet + '€');
+    if(x == y || y == z || x == z) {
+        win = +bet * +zweiGleich;
+        sessionStorage.setItem('money', +sessionStorage.getItem('money') + +win);
+        winLoseDisplay.textContent = "You won " + win + "€";
+        checkHighscore(win);
     }
     else{
         sessionStorage.setItem('money', +sessionStorage.getItem('money') - +bet)
+        winLoseDisplay.textContent = "Unfortunately you lost...";
     }
 }
 
 //Check if Player hat enough money to spin with entered bet
 function checkBet(bet){
     if(sessionStorage.getItem('money') < bet){
-        alert('You dont have enough money! Add some to your wallet to continue playing');
+        winLoseDisplay.textContent = "You don't have enough money, please charge some money to continue playing."
         return false;
     }
     return true;
 }
 
-
-
-const momentPerson = {
-    name: 'Nina',
-    email: 'ninaghg@gmx.com',
-    money: 28,
-    highscore: 0
+function checkHighscore(){
+    //TODO
 }
 
